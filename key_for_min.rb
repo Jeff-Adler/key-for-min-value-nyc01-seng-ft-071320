ebe6c6323ddb90bd5d36d683c08f0a33484d6f0b
Jeff Adler <jeffm.adler@gmail.com>
2020-06-11 17:56:06 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  temp_min_price = nil
+  temp_min_key = nil
+  name_hash.each do |item,price|
+      if temp_min_price == nil || price < temp_min_price
+        temp_min_price = price
+        temp_min_key = item
+      end
+  end
+  temp_min_key
 end
