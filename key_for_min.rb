3a76cb82ac326d13916ca5bd299d13971a601dde
Jeff Adler <jeffm.adler@gmail.com>
2020-06-11 17:53:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,8 @@
 def key_for_min_value(name_hash)
   temp_min_price = nil
   name_hash.each do |item,price|
-      if temp_min_price == nil || temp_min_price < price
+      if temp_min_price == nil || price < temp_min_price
         temp_min_price = price
+      end
   end
 end
